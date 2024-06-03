1ef5ecda99525c35d103a354891f117080f5866f
jitmo_0421 <jerram0421@gmail.com>
2024-06-03 10:25:41 +0800

    added another blank page for staffs

--- react-order-inventory-ts/src/pages/Staffs/Staffs.tsx
+++ react-order-inventory-ts/src/pages/Staffs/Staffs.tsx
@@ -1,7 +1,10 @@
+import List from '@/components/Staffs/List';
+
 const Staffs = () => {
   return (
     <div className="flex flex-col gap-4 my-4">
       <h1>Staffs</h1>
+      <List />
     </div>
   );
 };
