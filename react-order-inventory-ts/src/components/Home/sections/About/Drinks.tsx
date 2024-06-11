6e194832f1bd7688a00683fb0b407dc214def4cb
jitmo_0421 <jerram0421@gmail.com>
2024-06-11 12:37:38 +0800

    fixed positioning

--- react-order-inventory-ts/src/components/Home/sections/About/Drinks.tsx
+++ react-order-inventory-ts/src/components/Home/sections/About/Drinks.tsx
@@ -5,15 +5,17 @@ const Drinks = () => {
   return (
     <div className="w-full min-h-screen flex flex-col flex-wrap gap-10 justify-center items-center relative overflow-hidden">
       <div className="bg-[url('https://lgprkxqjhxzbuavhsdgr.supabase.co/storage/v1/object/public/assets/drop_lets.png?t=2024-06-10T03%3A23%3A51.108Z')] bg-cover bg-bottom opacity-20 absolute inset-0 z-[0]"></div>
-      <motion.h1
-        variants={fadeIn('down', 0.3)}
-        initial="hidden"
-        whileInView={'show'}
-        viewport={{ once: true, amount: 0.3 }}
-        className="w-full text-center"
-      >
-        A <span className="text-green-600">REFRESHING</span> <span className="text-orange-300">TREAT</span>.
-      </motion.h1>
+      <div className="flex flex-col justify-center items-center max-w-xl gap-2 inset-0 z-[1]">
+        <motion.h1
+          variants={fadeIn('down', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: true, amount: 0.3 }}
+          className="text-center"
+        >
+          A <span className="text-green-600">REFRESHING</span> <span className="text-orange-300">TREAT</span>.
+        </motion.h1>
+      </div>
       <div className="flex flex-col md:flex-row flex-wrap gap-12  inset-0 z-[1]">
         <motion.img
           variants={fadeIn('left', 0.3)}
