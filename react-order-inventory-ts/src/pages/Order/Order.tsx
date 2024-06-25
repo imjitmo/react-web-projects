8400e1123916ca19c6e54ad9023fcfc8a09410ba
jitmo_0421 <jerram0421@gmail.com>
2024-06-26 02:59:42 +0800

    updated create order user role

--- react-order-inventory-ts/src/pages/Order/Order.tsx
+++ react-order-inventory-ts/src/pages/Order/Order.tsx
@@ -1,14 +1,17 @@
 import CreateOrder from '@/components/Products/CreateOrder';
 import List from '@/components/Products/List';
+import { useStore } from '@/store/store';
+import { useShallow } from 'zustand/react/shallow';
 
 const Order = () => {
+  const { userType } = useStore(useShallow((state) => ({ userType: state.userType })));
   // const handleOrder = (order: Record<string, unknown>) => {
   //   console.log(order);
   // };
 
   return (
     <>
-      <CreateOrder />
+      {userType !== 'kitchen staff' && userType !== 'cashier' && <CreateOrder />}
       <List pageType={'order'} />
     </>
   );
