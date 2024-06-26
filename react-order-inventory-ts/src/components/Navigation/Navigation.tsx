39d4756c1b386139449a791af7b2f1c60a3b9a57
jitmo_0421 <jerram0421@gmail.com>
2024-06-26 22:07:43 +0800

    added logout confirmation

--- react-order-inventory-ts/src/components/Navigation/Navigation.tsx
+++ react-order-inventory-ts/src/components/Navigation/Navigation.tsx
@@ -10,8 +10,10 @@ import { Button } from '@/components/ui/button';
 import { useStaffLogout } from '@/hooks/use/useStaff';
 
 import { useStore } from '@/store/store';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useShallow } from 'zustand/react/shallow';
+import DialogTool from '../DialogTool';
 
 const Navigation = () => {
   const { logoutStaff } = useStaffLogout();
@@ -20,6 +22,8 @@ const Navigation = () => {
       userType: state.userType,
     }))
   );
+
+  const [onLogout, setOnLogout] = useState(false);
   const checkUserType = userType === 'admin' || userType === 'super' ? true : false;
   const notForKitchenAndCashier = userType !== 'kitchen staff' && userType !== 'cashier' ? true : false;
   const notForWaiterAndCashier = userType !== 'counter staff' && userType !== 'cashier' ? true : false;
@@ -27,109 +31,126 @@ const Navigation = () => {
     logoutStaff();
   };
   return (
-    <nav className="top-0 left-0 transform-[5rem] bg-slate-950/70 dark:bg-slate-100/20 min-w-[100px] min-h-screen backdrop-blur-2xl">
-      <div className="flex flex-col items-center justify-center gap-10">
-        <div className="flex items-center justify-center min-w-[70px] max-w-[80px] h-[100px]">
-          <Link to="/dashboard" className="p-2">
-            {/* <svg className="size-0">
+    <>
+      <nav className="top-0 left-0 transform-[5rem] bg-slate-950/70 dark:bg-slate-100/20 min-w-[100px] min-h-screen backdrop-blur-2xl">
+        <div className="flex flex-col items-center justify-center gap-10">
+          <div className="flex items-center justify-center min-w-[70px] max-w-[80px] h-[100px]">
+            <Link to="/dashboard" className="p-2">
+              {/* <svg className="size-0">
               <linearGradient id="blue-gradient" x1="0%" y1="100%" x2="0%" y2="0%">
                 <stop stopColor="#ea580c" offset="10%" />
                 <stop stopColor="#f97316" offset="100%" />
               </linearGradient>
             </svg>
             <BiSolidStoreAlt className="size-12" style={{ fill: 'url(#blue-gradient)' }} /> */}
-            <img
-              src="https://lgprkxqjhxzbuavhsdgr.supabase.co/storage/v1/object/public/assets/felicitas_logo.jpg"
-              alt="felicitas_logo"
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </Link>
-        </div>
-        <div className="w-full min-w-[50px] max-w-[50px] mx-auto flex flex-col justify-evenly items-center text-xl gap-8 text-white/50">
-          <NavLink
-            end
-            to="/dashboard"
-            className={({ isActive }) =>
-              `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
-            }
-          >
-            <GoHome className="size-7" />
-          </NavLink>
-          {notForKitchenAndCashier && (
-            <>
-              <NavLink
-                end
-                to="/order"
-                className={({ isActive }) =>
-                  `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
-                }
-              >
-                <IoRestaurantOutline className="size-7" />
-              </NavLink>
-              {notForWaiterAndCashier && (
+              <img
+                src="https://lgprkxqjhxzbuavhsdgr.supabase.co/storage/v1/object/public/assets/felicitas_logo.jpg"
+                alt="felicitas_logo"
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </Link>
+          </div>
+          <div className="w-full min-w-[50px] max-w-[50px] mx-auto flex flex-col justify-evenly items-center text-xl gap-8 text-white/50">
+            <NavLink
+              end
+              to="/dashboard"
+              className={({ isActive }) =>
+                `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
+              }
+            >
+              <GoHome className="size-7" />
+            </NavLink>
+            {notForKitchenAndCashier && (
+              <>
                 <NavLink
                   end
-                  to="/setup"
+                  to="/order"
                   className={({ isActive }) =>
                     `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
                   }
                 >
-                  <TbRowInsertTop className="size-7" />
+                  <IoRestaurantOutline className="size-7" />
                 </NavLink>
-              )}
-            </>
-          )}
-          <NavLink
-            end
-            to="/cook"
-            className={({ isActive }) =>
-              `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
-            }
-          >
-            <PiCookingPotBold className="size-7" />
-          </NavLink>
-          {notForWaiterAndCashier && (
+                {notForWaiterAndCashier && (
+                  <NavLink
+                    end
+                    to="/setup"
+                    className={({ isActive }) =>
+                      `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
+                    }
+                  >
+                    <TbRowInsertTop className="size-7" />
+                  </NavLink>
+                )}
+              </>
+            )}
             <NavLink
               end
-              to="/inventory"
+              to="/cook"
               className={({ isActive }) =>
                 `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
               }
             >
-              <MdOutlineInventory className="size-7" />
+              <PiCookingPotBold className="size-7" />
             </NavLink>
-          )}
-          {notForKitchenAndCashier && (
-            <NavLink
-              end
-              to="/customers"
-              className={({ isActive }) =>
-                `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
-              }
+            {notForWaiterAndCashier && (
+              <NavLink
+                end
+                to="/inventory"
+                className={({ isActive }) =>
+                  `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
+                }
+              >
+                <MdOutlineInventory className="size-7" />
+              </NavLink>
+            )}
+            {notForKitchenAndCashier && (
+              <NavLink
+                end
+                to="/customers"
+                className={({ isActive }) =>
+                  `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
+                }
+              >
+                <HiOutlineUserGroup className="size-7" />
+              </NavLink>
+            )}
+            {checkUserType && (
+              <NavLink
+                end
+                to="/users"
+                className={({ isActive }) =>
+                  `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
+                }
+              >
+                <LiaUsersCogSolid className="size-7" />
+              </NavLink>
+            )}
+            <Button
+              className="p-4 bg-transparent hover:bg-transparent rounded-lg text-orange-500"
+              onClick={() => setOnLogout((prev) => !prev)}
             >
-              <HiOutlineUserGroup className="size-7" />
-            </NavLink>
-          )}
-          {checkUserType && (
-            <NavLink
-              end
-              to="/users"
-              className={({ isActive }) =>
-                `p-4 rounded-lg ${isActive ? 'bg-orange-500 text-slate-50' : 'text-orange-500'}`
-              }
-            >
-              <LiaUsersCogSolid className="size-7" />
-            </NavLink>
-          )}
-          <Button
-            className="p-4 bg-transparent hover:bg-transparent rounded-lg text-orange-500"
-            onClick={() => handleLogout()}
-          >
-            <PiSignOutBold className="size-7" />
+              <PiSignOutBold className="size-7" />
+            </Button>
+          </div>
+        </div>
+      </nav>
+      <DialogTool
+        onOpen={onLogout}
+        setOnOpen={setOnLogout}
+        header="Logout"
+        description="Are you sure you want to logout?"
+      >
+        <div className="flex flex-row flex-wrap gap-4">
+          <Button className="grow" variant={'destructive'} onClick={handleLogout}>
+            Yes
+          </Button>
+          <Button className="bg-slate-500" onClick={() => setOnLogout((prev) => !prev)}>
+            No
           </Button>
         </div>
-      </div>
-    </nav>
+      </DialogTool>
+    </>
   );
 };
 
