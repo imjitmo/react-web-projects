31a40f8ef71070405a5fe8954f6a86e7e03951a8
jitmo_0421 <jerram0421@gmail.com>
2024-06-30 11:28:00 +0800

    reverted role

--- react-order-inventory-ts/src/components/Navigation/Navigation.tsx
+++ react-order-inventory-ts/src/components/Navigation/Navigation.tsx
@@ -93,7 +93,7 @@ const Navigation = () => {
             >
               <PiCookingPotBold className="size-7" />
             </NavLink>
-            {checkUserType && (
+            {notForWaiterAndCashier && (
               <NavLink
                 end
                 to="/inventory"
